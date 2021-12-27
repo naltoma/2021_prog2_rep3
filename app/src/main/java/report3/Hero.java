f9ca422bacd4ed880f1de30ed799237244702098
Naruaki TOMA <tnal@ie.u-ryukyu.ac.jp>
2021-12-27 10:58:16 +0900

    step 3

--- app/src/main/java/report3/Hero.java
+++ app/src/main/java/report3/Hero.java
@@ -9,10 +9,10 @@ package report3;
  * Created by tnal on 2016/11/13.
  */
 public class Hero {
-    public String name;
-    public int hitPoint;
-    public int attack;
-    public boolean dead;
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
@@ -28,6 +28,17 @@ public class Hero {
         System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
     }
 
+    //ゲッター
+    public String getName(){
+        return this.name;
+    }
+    public boolean isDead(){
+        return this.dead;
+    }  
+    public int getHitPoint(){
+        return this.hitPoint;
+    }  
+
     /**
      * Enemyへ攻撃するメソッド。
      * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
@@ -35,7 +46,7 @@ public class Hero {
      */
     public void attack(Enemy e){
         int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.name, damage);
+        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.getName(), damage);
         e.wounded(damage);
     }
 
